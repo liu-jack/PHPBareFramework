ab30637a4ad83c176749a7c81d43388896997657
camfee <camfee@foxmail.com>
2017-11-20 11:36:10 +0800

    phpdoc

--- Bat/phpdoc.sh
+++ Bat/phpdoc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-/usr/bin/php7.0 /home/camfee/app/phpdoc/phpDocumentor.phar -d /home/camfee/www/www.bare.com/Controller/Api -t /home/camfee/www-doc/www.bare.com/ --title "Bare API" --force --template=responsive-twig  --visibility="public"
+/usr/bin/php7.0 /home/camfee/app/phpdoc/bin/phpdoc -d /home/camfee/www/www.bare.com/Controller/Api -t /home/camfee/www-doc/www.bare.com/ --title "Bare API" --force --template=responsive-twig  --visibility="public"
