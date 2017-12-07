fe6877152fe8ed1b6938266412183f53cd0ece21
camfee <camfee@foxmail.com>
2017-12-07 21:26:30 +0800

    redis cache

---
+++ Data/redis_conf/redis.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+sudo redis-server /etc/redis/7379.conf
+sudo redis-server /etc/redis/7380.conf
+sudo redis-server /etc/redis/7381.conf
+sudo redis-server /etc/redis/7382.conf
+sudo redis-server /etc/redis/7383.conf
