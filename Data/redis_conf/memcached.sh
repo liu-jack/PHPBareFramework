5a3eca5b1b726aa60c8ee81e78258b7180c5a9f2
camfee <camfee@foxmail.com>
2017-12-14 11:06:33 +0800

    memcache cache

--- Data/redis_conf/memcached.sh
+++ Data/redis_conf/memcached.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo memcached -p 20001 -u memcache -m 1024 -c 2048 -A -l 127.0.0.1 &
-sudo memcached -p 20002 -u memcache -m 1024 -c 2048 -A -l 127.0.0.1 &
-sudo memcached -p 20003 -u memcache -m 1024 -c 2048 -A -l 127.0.0.1 &
+memcached -p 20001 -u memcache -m 1024 -c 2048 -A -l 127.0.0.1 &
+memcached -p 20002 -u memcache -m 1024 -c 2048 -A -l 127.0.0.1 &
+memcached -p 20003 -u memcache -m 1024 -c 2048 -A -l 127.0.0.1 &
 
